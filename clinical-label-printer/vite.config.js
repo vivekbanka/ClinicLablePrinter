9d15d277aed55b53f9ad5a950577413d3d8fce38
vivek <banka.vivekreddy@gmail.com>
2026-03-08 18:42:39 -0700

    Added clinical lable printer.

---
+++ clinical-label-printer/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "ClinicLabelPrinter",
+        short_name: "LabelPrinter",
+        display: "standalone", // Removes browser UI
+        theme_color: "#ffffff",
+        icons: [
+          { src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "pwa-512x512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
+  ],
+});
